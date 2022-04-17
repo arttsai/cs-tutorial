51b3277c5fc2535ed07bcaa9a7cbaec3e72603c6
Art Tsai <arttsai@gmail.com>
2022-04-17 17:02:18 +0800

    init.

---
+++ App1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var student = new Student();
+
+            Console.WriteLine(student.ToString());
+            Console.WriteLine(student.GetType());
+
+            var s3 = @"This text
+spawns multiple lines.";
+            Console.WriteLine(s3);
+
+            string s = null; 
+
+
+        }
+        
+    }
+
+    struct StructA
+    {
+        int a;
+        double d;
+        string str; 
+    }
+
+    internal class Student
+    {
+        public Student()
+        {
+        }
+    }
+}
