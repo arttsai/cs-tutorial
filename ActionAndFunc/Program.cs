2d95973e3b641b07cdbb25e0a2f6805ac3719131
Art Tsai <arttsai@gmail.com>
2022-05-08 12:24:55 +0800

    add more projects: ActionAndFunc, Functional

---
+++ ActionAndFunc/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ActionAndFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Func<double, double, double> Add = (a, b) => a + b;
+            Func<double, double, double> Del = (a, b) => a - b;
+            Func<double, double, double> Multi = (a, b) => a * b;
+            Func<double, double, double> Div = (a, b) => b == 0 ? 0 : (a / b); 
+
+            double value = Add(3, 5);
+            Console.WriteLine(value);
+            
+            value = Calculator(Add, 10, 20);
+            Console.WriteLine(value);
+
+            value = Calculator(Multi, 7, 9);
+            Console.WriteLine(value);
+
+            Console.WriteLine("Please enter your operation, and numbers, separated by comma");
+            var text = Console.ReadLine();
+            string[] inputs = text.Split(',');
+            foreach (var input in inputs)
+            {
+                Console.WriteLine(input);
+            }
+
+            var op = inputs[0];
+            double a = Convert.ToDouble(inputs[1]);
+            double b = Convert.ToDouble(inputs[2]);
+
+            var result = op switch
+            {
+                "add" => Calculator(Add, a, b),
+                "del" => Calculator(Del, a, b),
+                "multi" => Calculator(Multi, a, b),
+                "div" => Calculator(Div, a, b)
+            };
+            Console.WriteLine($"resule value = {result}");
+        }
+
+        public static double Calculator(Func<double, double, double> op, double a, double b)
+        {
+            return op(a, b); 
+        }
+    }
+}
---
+++ Functional/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var sb = new StringBuilder();
+            sb.AppendLine("this is the first line");
+            
+            for (int i = 0; i<10;  i++)
+            {
+                if (i % 2 == 0) sb.AppendLine($"{i} is an even number."); 
+            }
+
+            sb.AppendLine("this is the 2nd line.")
+                .AppendLine("this is the 3rd line."); 
+            var str = sb.ToString();
+
+            Console.WriteLine(str);
+        }
+    }
+}
